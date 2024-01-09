bb7803b3dfaeccf84496253043640c85cc34be67
lyukid0n <lyukidon@naver.com>
2024-01-09 17:38:35 +0900

    [edit] apiKey 값만 따로 분리

--- script/movieData.js
+++ script/movieData.js
@@ -1,5 +1,16 @@
 import { apiKey } from "./apiKey.js";
 
-export let movieData = fetch("https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1", apiKey)
+const options = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${apiKey}`,
+  },
+};
+
+export let movieData = fetch(
+  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+  options
+)
   .then((response) => response.json())
   .then((data) => data.results);
